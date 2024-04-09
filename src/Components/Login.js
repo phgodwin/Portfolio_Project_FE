27ec38f0cf2794abd72f9d119fde941f40628623
phgodwin <paige227@hotmail.co.uk>
2024-04-09 15:35:57 +0100

    initial commit, started log in function

---
+++ src/Components/Login.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import "./Login.css";
+
+function Login() {
+
+
+
+
+
+
+    return ( <form>
+
+        <input id="username" type="text"></input>
+        <input id="password" type=""></input>
+        <button type="submit">Log In</button>
+ 
+    </form>  );
+}
+
+export default Login;
