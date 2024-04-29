73b1966957c4db14b7acde2065c496b81fdf0810
phgodwin <paige227@hotmail.co.uk>
2024-04-29 17:49:43 +0100

    front page key frames

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes, BrowserRouter as Router, Link } from 'react-router-dom';
 import HomePage from './Components/Home/HomePage';
 import NewUser from './Components/NewUser';
 import TestPage from './Components/TestPage';
+import AboutPaige from './Components/About Me/AboutPaige';
 
 function App() {
 
@@ -13,12 +14,14 @@ function App() {
 
       <nav id="navbar">
         <Link id="nav" to='/'>Home</Link>
+        <Link id="nav" to='/AboutMe'>About Me</Link>
         <Link id="nav" to='/TestPage'>Test</Link>
       </nav>
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/NewUser' element={<NewUser />} />
         <Route path='/TestPage' element={<TestPage />} />
+        <Route path='/AboutMe' element={<AboutPaige/>} />
 
       </Routes>
     </Router>
---
+++ src/Components/About Me/AboutPaige.jsx
@@ -0,0 +1,14 @@
+function AboutPaige() {
+    return (
+        <main id="container">
+        <p>Hi I'm Paige,</p>
+        <section id="animation">
+          <div id="first"><div>Full stack engineer in training!</div></div>
+          <div id="second"><div>Coffeeholic!</div></div>
+          <div id="third"><div>Spotify's #1 fan!</div></div>
+        </section>
+      </main>
+    );
+}
+
+export default AboutPaige;
